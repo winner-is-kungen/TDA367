aa047deaa50d6aa6bd27bcbd90f0bff618b1e74c
Mårten Åsberg <marten.asberg@outlook.com>
2019-09-13 10:55:22 +0200

    Coordinates are now stored in Points

--- src/main/java/com/winner_is_kungen/tda367/view/canvas/IRenderable.java
+++ src/main/java/com/winner_is_kungen/tda367/view/canvas/IRenderable.java
@@ -1,7 +1,8 @@
 package com.winner_is_kungen.tda367.view.canvas;
 
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 
 public interface IRenderable {
-	void render(GraphicsContext gc, double offsetX, double offsetY, double zoom);
+	void render(GraphicsContext gc, Point2D offset, double zoom);
 }
--- src/main/java/com/winner_is_kungen/tda367/view/canvas/InfiniteCanvas.java
+++ src/main/java/com/winner_is_kungen/tda367/view/canvas/InfiniteCanvas.java
@@ -1,6 +1,7 @@
 package com.winner_is_kungen.tda367.view.canvas;
 
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseButton;
@@ -19,14 +20,11 @@ public class InfiniteCanvas extends Region {
 
 	private final ArrayList<IRenderable> elements = new ArrayList<IRenderable>();
 
-	private double offsetX = 0;
-	private double offsetY = 0;
+	private Point2D offset = new Point2D(0, 0);
 	private double zoomLevel = 1.0d;
 
-	/** Used for calculating the dela X when middle mouse dragging the canvas. */
-	private double mouseDragLastX = 0;
-	/** Used for calculating the dela Y when middle mouse dragging the canvas. */
-	private double mouseDragLastY = 0;
+	/** Used for calculating the dela X,Y when middle mouse dragging the canvas. */
+	private Point2D mouseDragLast = new Point2D(0, 0);
 
 	public InfiniteCanvas() {
 		this.getChildren().add(canvas);
@@ -41,13 +39,13 @@ public class InfiniteCanvas extends Region {
 		// Just an test object. To be removed.
 		addRenderable(new IRenderable() {
 			@Override
-			public void render(GraphicsContext gc, double offsetX, double offsetY, double zoom) {
+			public void render(GraphicsContext gc, Point2D offset, double zoom) {
 				gc.beginPath();
-				gc.moveTo(offsetX + (10 + 5) * zoom, offsetY + (10 + 5) * zoom);
-				gc.lineTo(offsetX + (10 - 5) * zoom, offsetY + (10 + 5) * zoom);
-				gc.lineTo(offsetX + (10 - 5) * zoom, offsetY + (10 - 5) * zoom);
-				gc.lineTo(offsetX + (10 + 5) * zoom, offsetY + (10 - 5) * zoom);
-				gc.lineTo(offsetX + (10 + 5) * zoom, offsetY + (10 + 5) * zoom);
+				gc.moveTo(offset.getX() + (10 + 10) * zoom, offset.getY() + (10 + 10) * zoom);
+				gc.lineTo(offset.getX() + (10 - 10) * zoom, offset.getY() + (10 + 10) * zoom);
+				gc.lineTo(offset.getX() + (10 - 10) * zoom, offset.getY() + (10 - 10) * zoom);
+				gc.lineTo(offset.getX() + (10 + 10) * zoom, offset.getY() + (10 - 10) * zoom);
+				gc.lineTo(offset.getX() + (10 + 10) * zoom, offset.getY() + (10 + 10) * zoom);
 				gc.stroke();
 			}
 		});
@@ -60,7 +58,7 @@ public class InfiniteCanvas extends Region {
 		graphics.clearRect(0, 0, this.getWidth(), this.getHeight());
 
 		for (IRenderable element : elements) {
-			element.render(graphics, offsetX, offsetY, zoomLevel);
+			element.render(graphics, offset, zoomLevel);
 		}
 	}
 
@@ -69,8 +67,7 @@ public class InfiniteCanvas extends Region {
 	 */
 	private void onMousePressed(MouseEvent event) {
 		if (event.getButton() == MouseButton.MIDDLE) {
-			mouseDragLastX = event.getX();
-			mouseDragLastY = event.getY();
+			mouseDragLast = new Point2D(event.getX(), event.getY());
 		}
 	}
 
@@ -79,10 +76,8 @@ public class InfiniteCanvas extends Region {
 	 */
 	private void onMouseDragged(MouseEvent event) {
 		if (event.getButton() == MouseButton.MIDDLE) {
-			offsetX = offsetX + (event.getX() - mouseDragLastX);
-			mouseDragLastX = event.getX();
-			offsetY = offsetY + (event.getY() - mouseDragLastY);
-			mouseDragLastY = event.getY();
+			offset = offset.add(event.getX() - mouseDragLast.getX(), event.getY() - mouseDragLast.getY());
+			mouseDragLast = new Point2D(event.getX(), event.getY());
 			render();
 		}
 	}
@@ -112,12 +107,30 @@ public class InfiniteCanvas extends Region {
 	}
 
 	/**
-	 * Resizes the canvas to cover the whole area and re redraws it.
+	 * Resizes the canvas to cover the whole area and redraws it.
 	 */
 	private void onSizeUpdate(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
 		canvas.setHeight(this.getHeight());
 		canvas.setWidth(this.getWidth());
 
 		render();
+	}
+
+	/**
+	 * Transforms a Point on this node to a Point in the virtual coordinate system.
+	 * @param point The local Point.
+	 * @return A Point in the local coordinate system.
+	 */
+	public Point2D localToCoordinates(Point2D point) {
+		return point.multiply(zoomLevel).add(offset);
+	}
+	/**
+	 * Transforms a Point on this node to a Point in the virtual coordinate system.
+	 * @param x The local x.
+	 * @param y The local y.
+	 * @return A Point in the local coordinate system.
+	 */
+	public Point2D localToCoordinates(double x, double y) {
+		return localToCoordinates(new Point2D(x, y));
 	}
 }

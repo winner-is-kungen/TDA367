f8e52a0e214a653d0a9da546723746757df1c06e
Mårten Åsberg <marten.asberg@outlook.com>
2019-09-15 15:23:05 +0200

    Dragging blocks
    Only works correctly at zoom factor 1.

--- src/main/java/com/winner_is_kungen/tda367/view/canvas/InfiniteCanvasBlock.java
+++ src/main/java/com/winner_is_kungen/tda367/view/canvas/InfiniteCanvasBlock.java
@@ -3,21 +3,81 @@ package com.winner_is_kungen.tda367.view.canvas;
 import javafx.beans.DefaultProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 @DefaultProperty("content")
 public class InfiniteCanvasBlock extends AnchorPane {
 	private final ObjectProperty<Node> content = new SimpleObjectProperty<Node>(this, "content");
 
+	private Point2D mouseDragStart;
+	private Point2D coordinateDragStart;
+
 	public InfiniteCanvasBlock() {
 		super();
+
+		setOnMousePressed(this::onMousePressed);
+		setOnMouseDragged(this::onMouseDragged);
 	}
 	public InfiniteCanvasBlock(Node content) {
 		this();
 		setContent(content);
 	}
 
+	private void onMousePressed(MouseEvent event) {
+		if (event.getButton() == MouseButton.PRIMARY) {
+			mouseDragStart = new Point2D(event.getSceneX(), event.getScreenY());
+			coordinateDragStart = new Point2D(getCoordinateX(), getCoordinateY());
+		}
+	}
+	private void onMouseDragged(MouseEvent event) {
+		if (event.getButton() == MouseButton.PRIMARY) {
+			Point2D mouse = new Point2D(event.getSceneX(), event.getScreenY());
+			Point2D delta = mouse.subtract(mouseDragStart);
+
+			Bounds layoutBounds = getLayoutBounds();
+			Point2D coordinatesMoved = new Point2D(
+					delta.getX() / (layoutBounds.getWidth() / getSizeX()),
+					delta.getY() / (layoutBounds.getHeight() / getSizeY())
+			);
+
+			setCoordinateX((int)Math.round(coordinateDragStart.getX() + coordinatesMoved.getX()));
+			setCoordinateY((int)Math.round(coordinateDragStart.getY() + coordinatesMoved.getY()));
+		}
+	}
+
+	//#region InfiniteCanvas properties
+	private int getCoordinateX() {
+		return InfiniteCanvas.getCoordinateX(this);
+	}
+	private void setCoordinateX(int value) {
+		InfiniteCanvas.setCoordinateX(this, value);
+	}
+	private int getCoordinateY() {
+		return InfiniteCanvas.getCoordinateY(this);
+	}
+	private void setCoordinateY(int value) {
+		InfiniteCanvas.setCoordinateY(this, value);
+	}
+	private int getSizeX() {
+		return InfiniteCanvas.getSizeX(this);
+	}
+	private void setSizeX(int value) {
+		InfiniteCanvas.setSizeX(this, value);
+	}
+	private int getSizeY() {
+		return InfiniteCanvas.getSizeY(this);
+	}
+	private void setSizeY(int value) {
+		InfiniteCanvas.setSizeY(this, value);
+	}
+	//#endregion InfiniteCanvas properties
+
+	//#region Content handling
 	public final Node getContent() {
 		return content.get();
 	}
@@ -41,4 +101,5 @@ public class InfiniteCanvasBlock extends AnchorPane {
 	public final ObjectProperty<Node> contentProperty() {
 		return content;
 	}
+	//#endregion Content handling
 }

69e92ee689d0836f1df90996dd127ba8c4499c60
Lukas <26006963+Peptski@users.noreply.github.com>
2019-09-19 17:51:33 +0200

    Added WorkspaceView
    Added WorkspaceView and controller for WorkspaceView

---
+++ src/main/java/com/winner_is_kungen/tda367/WorkspaceViewController.java
@@ -0,0 +1,23 @@
+package com.winner_is_kungen.tda367;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TabPane;
+
+import java.io.IOException;
+
+public class WorkspaceViewController extends TabPane {
+
+	public WorkspaceViewController() {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/WorkspaceView.fxml"));
+		fxmlLoader.setRoot(this);
+		fxmlLoader.setController(this);
+
+		try {
+			fxmlLoader.load();
+		}
+		catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+}

2422304336628d9a85d6efcf927e0703528da8ff
Deltaphish <joakimanderlind@gmail.com>
2019-09-20 17:09:53 +0200

    Added documentation for ComponentListener.java

--- src/main/java/com/winner_is_kungen/tda367/model/ComponentListener.java
+++ src/main/java/com/winner_is_kungen/tda367/model/ComponentListener.java
@@ -1,5 +1,13 @@
 package com.winner_is_kungen.tda367.model;
 
+/**
+ *  An interface for receiving updates from components
+ */
 public interface ComponentListener{
+	/**
+	 * When connected to another component, it will be called when the value of the component changes
+	 * @param val   The new value from output
+	 * @param channel The target input
+	 */
 	void update(boolean val, int channel);
 }

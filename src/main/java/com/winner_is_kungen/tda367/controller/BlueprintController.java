88f61df3cc0d71650af4fefc31500c65093e0ae7
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-01 18:40:10 +0200

    Component Controller and accompanying factory

--- src/main/java/com/winner_is_kungen/tda367/controller/BlueprintController.java
+++ src/main/java/com/winner_is_kungen/tda367/controller/BlueprintController.java
@@ -1,28 +1,33 @@
 package com.winner_is_kungen.tda367.controller;
 
+import com.winner_is_kungen.tda367.model.Blueprint;
+import com.winner_is_kungen.tda367.model.Component;
 import com.winner_is_kungen.tda367.view.canvas.InfiniteCanvas;
 
 public class BlueprintController extends InfiniteCanvas {
-	/*private Blueprint blueprint;*/
+	private Blueprint blueprint;
 
 	/**
 	 * Sets which Blueprint this controller should display and interact with.
 	 */
-	public void setBlueprint(/*Blueprint blueprint*/) {
-		/*this.blueprint = blueprint;*/
+	public void setBlueprint(Blueprint blueprint) {
+		this.blueprint = blueprint;
 
-		getManagedChildren().clear();
-		/*for (ElectricalComponent component : this.blueprint.components) {
-			getManagedChildren().add(ElectricalComponentControllerFactory.Create(component));
-		}*/
+		getChildren().clear();
+
+		if (blueprint != null) {
+			for (int i = 0; i < blueprint.getSize(); i++) {
+				getChildren().add(ComponentControllerFactory.Create(blueprint.getComponent(i)));
+			}
+		}
 	}
 
 	/**
 	 * Adds a new component to this controllers Blueprint.
 	 * @param component The component to be added.
 	 */
-	public void addComponent(/*Electrical component*/) {
-		getManagedChildren().add(ElectricalComponentControllerFactory.Create(/*component*/));
-		/*blueprint.addComponent(component);*/
+	public void addComponent(Component component) {
+		getChildren().add(ComponentControllerFactory.Create(component));
+		blueprint.addComponent(component);
 	}
 }
---
+++ src/main/java/com/winner_is_kungen/tda367/controller/ComponentControllerFactory.java
@@ -0,0 +1,26 @@
+package com.winner_is_kungen.tda367.controller;
+
+import com.winner_is_kungen.tda367.model.Component;
+import javafx.scene.image.Image;
+
+import java.util.Map;
+
+public class ComponentControllerFactory {
+	/** A map containing type ids and their corresponding icons. */
+	private static final Map<String, String> gateIcons = Map.ofEntries(
+//			Map.entry("test", "/gateIcons/test.png")
+	);
+
+	/** The default icon for unknown gates. */
+	private static final String defaultGateIcon = "/gateIcons/unknown.png";
+
+	/**
+	 * Creates a controller for the given model. Chooses controller based on the type of the model.
+	 * @param model The model that the controller should display.
+	 * @return A controller that displays the model.
+	 */
+	public static ComponentController Create(Component model) {
+		Image image = new Image(ComponentControllerFactory.class.getResourceAsStream(gateIcons.getOrDefault("test" /*model.type*/, defaultGateIcon)));
+		return new ComponentController(model, image);
+	}
+}

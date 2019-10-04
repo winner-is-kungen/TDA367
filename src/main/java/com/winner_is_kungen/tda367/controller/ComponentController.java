69e833cf3da60f0fc7f645c56a15042e9f0d8bdb
Joakim <joakimanderlind@gmail.com>
2019-10-04 18:31:58 +0200

    Added styling to Component in view

--- src/main/java/com/winner_is_kungen/tda367/controller/ComponentController.java
+++ src/main/java/com/winner_is_kungen/tda367/controller/ComponentController.java
@@ -10,7 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Paint;
+import javafx.scene.paint.*;
 import javafx.scene.shape.Circle;
 
 import java.io.IOException;
@@ -25,6 +25,9 @@ public class ComponentController extends InfiniteCanvasBlock {
 
 	@FXML
 	private VBox output_connections;
+
+	private final Paint default_connector = new RadialGradient(0.0,0.0,0.0,0.0,20.0,false, CycleMethod.NO_CYCLE,new Stop(0, Color.web("rgba(184,184,184,1)")),new Stop(1,Color.web("#49ff00")));
+	private final double connectionRadius = 8.0;
 
 	/** The model of the Component this displays. */
 	private final Component model;
@@ -57,12 +60,12 @@ public class ComponentController extends InfiniteCanvasBlock {
 
 		int nrInputs = this.model.getNrInputs();
 		for(int i = 0; i != nrInputs;i++){
-			input_connections.getChildren().add(new Circle(0,0,10, Paint.valueOf("#DEADBEEF")));
+			input_connections.getChildren().add(new Circle(0,0,connectionRadius,default_connector));
 		}
 
 		int nrOutputs = this.model.getNrOutputs();
 		for(int i = 0; i != nrOutputs;i++){
-			output_connections.getChildren().add(new Circle(0,0,10, Paint.valueOf("#DEADBEEF")));
+			output_connections.getChildren().add(new Circle(0,0,connectionRadius,default_connector));
 		}
 
 		// Controller setup

697a8cceff297ed296d8f92f863d3c97f5d339f4
Obada Al Khayat <obadaa@student.chalmers.se>
2019-10-19 15:52:36 +0200

    Added TextInputDialog for choosing name of new files

--- src/main/java/com/winner_is_kungen/tda367/controller/MainController.java
+++ src/main/java/com/winner_is_kungen/tda367/controller/MainController.java
@@ -3,9 +3,11 @@ package com.winner_is_kungen.tda367.controller;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class MainController extends AnchorPane {
 
@@ -37,7 +39,18 @@ public class MainController extends AnchorPane {
 	private void menuBarItemsHandler(MenuBarController.MenuItemEvent event) {
 
 		if (event.getMenuItem() == "newFile") {
-			workspaceviewController.createNewFile();
+
+			TextInputDialog chooseName = new TextInputDialog();
+			chooseName.setTitle("Please enter the name of the file");
+			chooseName.setHeaderText("New File...");
+			chooseName.setContentText("Please enter file name:");
+
+			Optional<String> result = chooseName.showAndWait();
+			if (result.isPresent() && result.get().strip().length() >= 1) {
+				workspaceviewController.createNewFile(result.get());
+			}
+
+
 		} else if (event.getMenuItem() == "saveFile") {
 			//TODO save function
 
--- src/main/java/com/winner_is_kungen/tda367/controller/WorkspaceViewController.java
+++ src/main/java/com/winner_is_kungen/tda367/controller/WorkspaceViewController.java
@@ -3,20 +3,24 @@ package com.winner_is_kungen.tda367.controller;
 import com.winner_is_kungen.tda367.model.Blueprint;
 import com.winner_is_kungen.tda367.model.Component;
 import com.winner_is_kungen.tda367.model.LogicGates.ComponentFactory;
+import com.winner_is_kungen.tda367.model.Workspace;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WorkspaceViewController extends TabPane {
 
 	@FXML
-	private TabPane workspaceView;
-
-	@FXML
 	private BlueprintController blueprintController;
+
+	private Workspace workspace;
 
 	public WorkspaceViewController() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/WorkspaceView.fxml"));
@@ -29,7 +33,26 @@ public class WorkspaceViewController extends TabPane {
 			throw new RuntimeException(ex);
 		}
 
-		this.blueprintController.setBlueprint(new Blueprint());
+		Map<String, Blueprint> blueprintMap = new HashMap<>();
+		blueprintMap.put("new file", new Blueprint());
+
+		workspace = new Workspace(blueprintMap);
+
+		blueprintController.setBlueprint(workspace.getBlueprint("new file"));
+
+		this.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>() {
+			@Override
+			public void changed(ObservableValue<? extends Tab> observable, Tab oldTab, Tab newTab) {
+
+				String fileName = newTab.getText();
+				System.out.println(fileName);
+
+				blueprintController.setBlueprint(workspace.getBlueprint(fileName));
+
+				newTab.setContent(blueprintController);
+			}
+		});
+
 	}
 
 	public void addNewComponent(String typeID) {
@@ -37,12 +60,20 @@ public class WorkspaceViewController extends TabPane {
 		blueprintController.addComponent(newComp);
 	}
 
-	public void createNewFile() {
+	public void createNewFile(String fileName) {
 
 		Tab tab = new Tab();
-		tab.setText("Untitled Tab");
+		tab.setId(fileName);
+		tab.setText(fileName);
+
+		Blueprint newBlueprint = new Blueprint();
+		workspace.addBlueprint(fileName, newBlueprint);
+
+		Tab oldTab = this.getSelectionModel().getSelectedItem();
+		System.out.println("selected tab " + oldTab.getText());
 		tab.setContent(blueprintController);
-		workspaceView.getTabs().add(tab);
+
+		getTabs().add(tab);
 
 	}
 

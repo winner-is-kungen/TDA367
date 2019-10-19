0c81289d3a3136aa69ad3299c9d008f43c182fe8
Joakim <joakimanderlind@gmail.com>
2019-10-19 20:42:58 +0200

    Changed from inheritance to composition

--- src/main/java/com/winner_is_kungen/tda367/model/Signal.java
+++ src/main/java/com/winner_is_kungen/tda367/model/Signal.java
@@ -3,16 +3,35 @@ package com.winner_is_kungen.tda367.model;
 import com.winner_is_kungen.tda367.model.util.Tuple;
 
 import java.util.ArrayList;
+import java.util.List;
 
-class Signal extends ArrayList<Tuple<ComponentListener,Integer,Integer>> {
+class Signal {
+
+	List<Tuple<ComponentListener,Integer,Integer>> listeners = new ArrayList<>();
 
 	void broadcastUpdate(String updateID, boolean[] newValues){
-		for(Tuple<ComponentListener,Integer,Integer> connection : this){
+		for(Tuple<ComponentListener,Integer,Integer> connection : listeners){
 
 			boolean outputValue = newValues[connection.third()];
 			int inputChannel = connection.second();
 
 			connection.first().update(updateID, outputValue, inputChannel);
 		}
+	}
+
+	public void add(Tuple<ComponentListener, Integer, Integer> newListener) {
+		listeners.add(newListener);
+	}
+
+	public void remove(Tuple<ComponentListener, Integer, Integer> listener) {
+		listeners.remove(listener);
+	}
+
+	public int size() {
+		return listeners.size();
+	}
+
+	public Tuple<ComponentListener, Integer, Integer> get(int index) {
+		return listeners.get(index);
 	}
 }

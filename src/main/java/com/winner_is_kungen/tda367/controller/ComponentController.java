26b5c99d17759151824a4c081d648c251ec97332
Obada Al Khayat <obadaa@student.chalmers.se>
2019-10-24 12:23:08 +0200

    Merge conflict fix

--- src/main/java/com/winner_is_kungen/tda367/controller/ComponentController.java
+++ src/main/java/com/winner_is_kungen/tda367/controller/ComponentController.java
@@ -35,7 +35,7 @@ public class ComponentController extends InfiniteCanvasBlock {
 	private ConnectionPointController[] inputs;
 	private ConnectionPointController[] outputs;
 
-	ComponentController(Component model, String symbol) {
+	public ComponentController(Component model, String symbol) {
 		// FXML setup
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Component.fxml"));
 		fxmlLoader.setRoot(this);
